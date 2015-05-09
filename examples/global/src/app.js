b1cf551e4e42ef5517d98f4a66198ea83850aecb
Riccardo Coppola <ricca509@gmail.com>
2015-05-09 17:25:35 +0100

    Refactors examples

---
+++ examples/global/src/app.js
@@ -0,0 +1,13 @@
+(function (Marionette) {
+    'use strict';
+
+    var App = new Marionette.Application();
+
+    App.addRegions({
+        menu: '[data-region="menu"]',
+        content: '[data-region="content"]',
+        modal: Marionette.Region.Modal.extend({ el: '[data-region="modal"]' })
+    });
+
+    window.App = App;
+}(window.Marionette));
---
+++ examples/global/src/init.js
@@ -0,0 +1,48 @@
+(function(Marionette, App) {
+    'use strict';
+
+    App.start();
+
+    var renderAsModal = function () {
+        render(App.modal, new ItemDetails());
+    };
+
+    var renderAsContent = function () {
+        render(App.content, new ItemDetails());
+    };
+
+    var emptyContent = function () {
+        App.content.empty();
+    };
+
+    var render = function (region, view) {
+        region.show(view);
+    };
+
+    var Menu = Marionette.ItemView.extend({
+        template: '#menu-tmpl',
+        triggers: {
+            'click a[data-role="open-modal"]': 'item:modal:click',
+            'click a[data-role="open-inline"]': 'item:inline:click',
+            'click a[data-role="close-inline"]': 'item:inline:close:click'
+        }
+    });
+
+    var ItemDetails = Marionette.ItemView.extend({
+        template: '#item-details-tmpl',
+        events: {
+            'click button[type="submit"]': 'submit'
+        },
+        submit: function (ev) {
+            ev.preventDefault();
+            console.log('submit');
+        }
+    });
+
+    var menu = new Menu();
+    menu.on('item:modal:click', renderAsModal);
+    menu.on('item:inline:click', renderAsContent);
+    menu.on('item:inline:close:click', emptyContent);
+
+    App.menu.show(menu);
+}(window.Marionette, window.App));
